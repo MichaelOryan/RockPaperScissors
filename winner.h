ef0cfacccd6de913a4bdfd9545bd6163a292fb93
Michael O'Ryan <michael.oryan@gmail.com>
2015-01-05 18:40:09 +1100

    Create winner.h
    Code to determine the winner in a balanced game of rock, paper, scissors, ... game of n number of choices.

---
+++ winner.h
@@ -0,0 +1,16 @@
+// -1 = lose, 0 = draw, 1 = win
+int didIWin(int myPick, int otherPick, int numChoices)
+{
+  int result = (myPick - otherPick) % numChoices;
+  
+  int partition = numChoices / 2;
+  if(result == partition && numChoices % 2 == 0)
+    result = 0;
+  
+  if(result == 0)
+    return 0;
+  else if(result > partition)
+    return -1;
+  else
+    return 1;
+}
